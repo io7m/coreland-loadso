cdf513257f386cbfb651d6a9be015043fc19fdd4
m0 <m0@viper>
2010-03-11 04:44:12 +0000

    Explicit pre/postconditions

--- loadso.c
+++ loadso.c
@@ -24,57 +24,92 @@ loadso_set_error (const char *e)
 int
 loadso_function (loadso_handle_t handle, const char *symbol, loadso_pointer_t *function)
 {
+  int r = 0;
+
+  /* Preconditions */
   assert (handle   != NULL);
   assert (symbol   != NULL);
   assert (function != NULL);
 
 #if defined(SD_HAVE_DLOPEN)
-  return loadso_dlopen_function (handle, symbol, function);
+  r = loadso_dlopen_function (handle, symbol, function);
 #elif SD_SYSINFO_OS == SD_SYSINFO_OS_MS_WINDOWS
-  return loadso_win32_function (handle, symbol, function);
-#endif
-
+  r = loadso_win32_function (handle, symbol, function);
+#else
   loadso_set_error ("function not implemented on this platform");
-  return 0;
+#endif
+  if (r == 0) *function = NULL;
+
+  /* Postconditions */
+  assert (r >= 0);
+  assert (r <= 1);
+  if (r == 1) assert (*function != NULL);
+  if (r == 0) assert (*function == NULL);
+
+  return r;
 }
 
 int
 loadso_open (const char *file, loadso_handle_t *handle)
 {
+  int r = 0;
+
+  /* Preconditions */
   assert (file   != NULL);
   assert (handle != NULL);
 
 #if defined(SD_HAVE_DLOPEN)
-  return loadso_dlopen_open (file, handle);
+  r = loadso_dlopen_open (file, handle);
 #elif SD_SYSINFO_OS == SD_SYSINFO_OS_MS_WINDOWS
-  return loadso_win32_open (file, handle);
-#endif
-
+  r = loadso_win32_open (file, handle);
+#else
   loadso_set_error ("function not implemented on this platform");
-  return 0;
+#endif
+  if (r == 0) *handle = NULL;
+
+  /* Postconditions */
+  assert (r >= 0);
+  assert (r <= 1);
+  if (r == 1) assert (*handle != NULL);
+  if (r == 0) assert (*handle == NULL);
+
+  return r;
 }
 
 int
 loadso_symbol (loadso_handle_t handle, const char *symbol, void **pointer)
 {
+  int r = 0;
+
+  /* Preconditions */
   assert (handle  != NULL);
   assert (symbol  != NULL);
   assert (pointer != NULL);
 
 #if defined(SD_HAVE_DLOPEN)
-  return loadso_dlopen_symbol (handle, symbol, pointer);
+  r = loadso_dlopen_symbol (handle, symbol, pointer);
 #elif SD_SYSINFO_OS == SD_SYSINFO_OS_MS_WINDOWS
-  return loadso_win32_symbol (handle, symbol, pointer);
-#endif
-
+  r = loadso_win32_symbol (handle, symbol, pointer);
+#else
   loadso_set_error ("function not implemented on this platform");
-  return 0;
+#endif
+  if (r == 0) *pointer = NULL;
+
+  /* Postconditions */
+  assert (r >= 0);
+  assert (r <= 1);
+  if (r == 1) assert (*pointer != NULL);
+  if (r == 0) assert (*pointer == NULL);
+
+  return r;
 }
 
 int
 loadso_close (loadso_handle_t *handle)
 {
   int r = 0;
+
+  /* Preconditions */
   assert (handle != NULL);
 
 #if defined(SD_HAVE_DLOPEN)
@@ -84,7 +119,13 @@ loadso_close (loadso_handle_t *handle)
 #else
   loadso_set_error ("function not implemented on this platform");
 #endif
+  if (r == 1) *handle = NULL;
 
-  if (r != 0) *handle = NULL;
+  /* Postconditions */
+  assert (r >= 0);
+  assert (r <= 1);
+  if (r == 1) assert (*handle == NULL);
+  if (r == 0) assert (*handle != NULL);
+
   return r;
 }
